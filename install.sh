f25ba7373aa3609b824fcc639ec7487b6be1a352
Stefan Collier <sc22g13@soton.ac.uk>
2016-10-25 15:05:08 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ cd ~
 mkdir SaturnServer && true
 cd SaturnServer
 
-wget http://degas.ecs.soton.ac.uk/~productizer/Googlenet_791113_192patch.prm
+wget http://seurat.ecs.soton.ac.uk/~productizer/Googlenet_791113_192patch.prm
 
 mkdir images && true
 
