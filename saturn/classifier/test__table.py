b7ad81afa6b4ed4533d0e052b9358f2bf54ba518
Stefan Collier <sc22g13@soton.ac.uk>
2016-10-28 16:40:52 +0100

    Fixed test_get_a;;_features assertion to 5

--- saturn/classifier/test__table.py
+++ saturn/classifier/test__table.py
@@ -10,8 +10,7 @@ class TableUnitTest(unittest.TestCase):
         self.assertEqual(self.tab.add_feature('lake'), True)
 
     def test_get_all_feature(self):
-        self.assertEqual(len(self.tab.find_all_features()), 1)
+        self.assertEqual(len(self.tab.find_all_features()), 5)
 
 if __name__ == "__main__":
-    suite = unittest.TestLoader().loadTestsFromTestCase(TableUnitTest)
-    unittest.TextTestRunner(verbosity=2).run(suite)
+    unittest.main()
