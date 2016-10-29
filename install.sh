4cf56b9e00cbda4ce292e62e110b1267f905730c
Stefan Collier <sc22g13@soton.ac.uk>
2016-10-29 18:08:15 +0100

    Updated the installer to include test_resources

--- install.sh
+++ install.sh
@@ -9,24 +9,48 @@
 #       $./install.sh
 #
 
-# Make the file that runs the application
-runner=~/start_saturn.sh
-rm $runner && true
-touch $runner
-
 function write {
     echo $1 >> $runner
 }
 
+# $1 -- location of file to download
+# $2 -- Local dest, where to store file
+# $3 -- descriptor of what file is (in case of problem with downloading)
+function download {
+    filename=$(echo $1 | sed 's:.*\/::g')
+    dest_file=${2}/${filename}
+    if [ -e $dest_file ]; then
+        echo "${3} already downloaded at ${dest_file}"
+    else
+        cd $2 1>/dev/null
+        wget $1
+        cd - 1>/dev/null
+    fi
+}
+
+
+# Make the file that runs the application
+runner=~/start_saturn.sh
+rm $runner 2>/dev/null
+touch $runner
 write '#!/bin/bash'
 write ''
 write "python ${PWD}/saturn/saturn.py"
 write ''
 chmod +x $runner
 
-mkdir ~/SaturnServer && true
-cd ~/SaturnServer
-wget http://seurat.ecs.soton.ac.uk/~productizer/Googlenet_791113_192patch.prm
+# Make the SaturnServer directory tree
+mkdir ~/SaturnServer 2>/dev/null
+mkdir ~/SaturnServer/test_resources 2>/dev/null
+mkdir ~/SaturnServer/images 2>/dev/null
 
-mkdir ~/SaturnServer/images && true
+# Download the files needed to run and test the server
+download 'http://degas.ecs.soton.ac.uk/~productizer/test_resources/test_tile.jpg' ~/SaturnServer/test_resources 'test_tile.jpg'
+download 'http://seurat.ecs.soton.ac.uk/~productizer/Googlenet_791113_192patch.prm' ~/SaturnServer 'prm file'
 
+# Ensure that the prm file is downloaded!
+if [ -e ~/SaturnServer/Googlenet_791113_192patch.prm ] ; then
+    echo -e "\e[32m-- Saturn Fully Installed --\e[39m"
+else
+    echo -e "\e[31m-- Saturn Installation FAILED --\e[39m"
+fi
--- saturn/olivia/test__vectorizer.py
+++ saturn/olivia/test__vectorizer.py
@@ -1,10 +1,32 @@
 import unittest
 from vectorizer import Vectorizer
+import os
+
+
+default_prm_path = os.path.expanduser('~')+'/SaturnServer/Googlenet_791113_192patch.prm'
+
+image_loc =  os.path.expanduser('~')+'/SaturnServer/test_resources/windmill.jpg'
 
 class VectorizerTest(unittest.TestCase):
 
-    def test_(self):
-        return True
+    def test_regression__vectorizer_layer_minus_one_behaves_same(self):
+        # GIVEN a layer to test
+        layer_under_test = -1
+
+        # AND a vectorizer that uses that layer
+        vec = Vectorizer(layer=layer_under_test, prm_path=default_prm_path)
+
+        # AND an expected output
+        expected_output = []
+
+        #
+        # WHEN extracting the attributes from an image
+        actual_output = vec.get_attribute_vector(image_loc)
+
+        #
+        # THEN the output is as expected
+        self.assertEqual(expected_output, actual_output, 'The output %s, does not match the expected output of %s' % (str(actual_output), str(expected_output)))
+
 
 
 
