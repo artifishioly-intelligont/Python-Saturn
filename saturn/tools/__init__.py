5c5c0accb725352dead6615284964bba46d7b6c1
Stefan Collier <sc22g13@soton.ac.uk>
2016-11-16 20:53:19 +0000

    Fixed import in __init__

--- saturn/tools/__init__.py
+++ saturn/tools/__init__.py
@@ -3,7 +3,7 @@ This module is reserved for arbitrary helper functions
 """
 import os
 
-from downloader import download, download_image
+from downloader import download
 from image_handler import ImageHandler
 
 images = ImageHandler('%s/SaturnServer/images/' % os.path.expanduser('~'))
